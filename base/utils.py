70f373f9ead28911e4ba51c5e48e4f20b3d7376a
adwait-thattey <coderdude1999@gmail.com>
2019-11-20 23:44:03 +0530

    add num only option to unique_generator

--- base/utils.py
+++ base/utils.py
@@ -1,9 +1,10 @@
 import random
 
 
-def generate_random_string(length=10):
+def generate_random_string(length=10, num_only=False):
     choices = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
+    if num_only:
+        choices = "0123456789"
     selected_arr = [random.choice(choices) for _ in range(length)]
 
     rand_string = "".join(selected_arr)
