e1c30db457ef80f2a94b2f48f10163c096e57896
adwait-thattey <coderdude1999@gmail.com>
2019-11-21 00:03:14 +0530

    Setup basic login page

---
+++ registration/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+app_name = "registration"
+
+urlpatterns = [
+
+
+    # path('signup/', views.signup,name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+]
