057aff6a6cd1f02fa1f055aca4acd089834338cc
adwait-thattey <coderdude1999@gmail.com>
2019-11-21 00:03:30 +0530

    setup entry logs page

--- entry_logs/views.py
+++ entry_logs/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+@login_required
+def show_logs(request):
+    return HttpResponse("These are the Entry Logs")
