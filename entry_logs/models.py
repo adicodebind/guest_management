83fb4ac0cde91729386515295b4eb15b97d59393
adwait-thattey <coderdude1999@gmail.com>
2019-11-21 02:12:16 +0530

    model updates

--- entry_logs/models.py
+++ entry_logs/models.py
@@ -6,13 +6,17 @@ from django.utils import timezone
 
 
 # Create your models here.
+def get_visitor_token():
+    return generate_random_string(5, num_only=True)
+
+
 class VisitorLog(models.Model):
     visitor_user = models.ForeignKey(to=Visitor, on_delete=models.CASCADE)
     public_id = models.CharField(max_length=settings.PUBLIC_ID_LENGTH, editable=False, unique=True)
     address = models.TextField()
     checkin_time = models.DateTimeField(default=timezone.now)
     checkout_time = models.DateTimeField(null=True)
-    unique_token_number = models.CharField(max_length=4, default=generate_random_string(5, num_only=True))
+    unique_token = models.CharField(max_length=4, default=get_visitor_token)
 
     def save(self, *args, **kwargs):
         if not self.public_id:
