794f3f281f2ec7dad678888f6f041a9e868d2bf1
adwait-thattey <coderdude1999@gmail.com>
2019-11-21 11:50:45 +0530

    signup sequence completed

--- registration/urls.py
+++ registration/urls.py
@@ -6,8 +6,9 @@ app_name = "registration"
 urlpatterns = [
 
 
-    # path('signup/', views.signup,name='signup'),
+    path('signup/', views.signup,name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from registration.forms import RegisterForm
+from registration.models import User, Host
+
+
+def signup(request):
+    if request.method == "POST":
+        new_user_form = RegisterForm(request.POST)
+        if new_user_form.is_valid():
+            new_user = User.objects.create_user(
+                    username=new_user_form.cleaned_data["email"],
+                    password=new_user_form.cleaned_data["password"],
+                    email=new_user_form.cleaned_data["email"]
+                )
+
+            new_user.first_name = new_user_form.cleaned_data["first_name"]
+            new_user.last_name = new_user_form.cleaned_data["last_name"]
+            new_user.save()
+            host = Host.objects.create(user=new_user)
+            login(request, new_user)
+            return redirect('/')
+        else:
+            return render(request, 'registration/register.html', {"signup_form": new_user_form})
+    else:
+        new_form = RegisterForm()
+        return render(request, 'registration/register.html', {"signup_form": new_form})
