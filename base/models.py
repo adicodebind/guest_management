4558a58f88e4fbe7e48b8fbae8e3ae9a168b9496
adwait-thattey <coderdude1999@gmail.com>
2019-11-21 11:12:46 +0530

    updates

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class EmailLog(models.Model):
+    sender = models.CharField(max_length=100)
+    targets = models.TextField()  # comma seperated email IDs
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
--- base/utils.py
+++ base/utils.py
@@ -1,4 +1,18 @@
 import random
+import threading
+
+from django.core.mail import send_mail
+
+from base.models import EmailLog
+
+
+def run_in_background(func):
+    def decorator(*args, **kwargs):
+        t = threading.Thread(target=func, args=args, kwargs=kwargs)
+        t.daemon = True
+        t.start()
+
+    return decorator
 
 
 def generate_random_string(length=10, num_only=False):
@@ -19,3 +33,13 @@ def generate_public_id(object, length=10):
         rand_string = generate_random_string()
 
     return rand_string
+
+
+def send_email_wrapper(sender, receivers, subject, message):
+    send_mail(subject=subject,
+              message=message,
+              from_email=sender,
+              recipient_list=tuple(receivers),
+              fail_silently=False
+              )
+    EmailLog.objects.create(sender=sender, targets=",".join(receivers), subject=subject, content=message)
